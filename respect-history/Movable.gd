58242ee3c05b366bb190b6a91417b9cb74302011
Wix1410 <grautyzm11wspolprace@gmail.com>
2026-03-18 15:23:48 +0100

    Movable falling complete

--- respect-history/Movable.gd
+++ respect-history/Movable.gd
@@ -1,8 +1,8 @@
-extends Area2D
+extends CharacterBody2D
 
 @export var speed = 500
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	if collision_mask == 1:
-		position.y = speed * delta
+	velocity = Vector2(0,speed)
+	move_and_slide()
